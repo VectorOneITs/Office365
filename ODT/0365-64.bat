663d66f0f0a48ba3bce4a6979bfb83ee23e1f0fe
VectorOneITs <support@vectorone-its.com>
2025-01-24 14:11:15 -0500

    add config files
    add files for office 365, include following configurations:
    1 - Office 365
    2 - Office 365 + VIsio

---
+++ ODT/0365-64.bat
@@ -0,0 +1,7 @@
+##Office365 64bit##
+setup.exe /download O365_x64.xml
+setup.exe /configure O365_x64.xml
+
+##Office365 and Visio 64bit##
+#setup.exe /download O365_Visio_x64.xml
+#setup.exe /configure O365_Visio_x64.xml
